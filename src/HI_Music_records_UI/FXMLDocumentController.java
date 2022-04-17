f0a9c0113cb1a7bdcce051505b1fcaf548eef6c5
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-04-17 22:31:25 +0200

    added navigation

--- src/HI_Music_records_UI/FXMLDocumentController.java
+++ src/HI_Music_records_UI/FXMLDocumentController.java
@@ -5,14 +5,20 @@
  */
 package HI_Music_records_UI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -35,6 +41,26 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private void ExitButtonAction(ActionEvent event){
         System.exit(0);
+    }
+    
+    @FXML 
+    private Button signUpButton;
+    
+    @FXML
+    private void SignUpAction() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLRegistration.fxml"));
+        Stage stage = (Stage) signUpButton.getScene().getWindow();
+        stage.setScene(new Scene(root));
+    }
+    
+    @FXML 
+    private Button toSignIn;
+    
+    @FXML
+    private void toSignInAction() throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Stage stage = (Stage) toSignIn.getScene().getWindow();
+        stage.setScene(new Scene(root));
     }
     
     @Override
