95c412ab6743f073a9f35e2b37897512d08c4bae
Sango <60422984+MatyilaSango@users.noreply.github.com>
2022-05-30 22:27:55 +0200

    configured create page

--- src/HI_Music_records_UI/FXMLCreateController.java
+++ src/HI_Music_records_UI/FXMLCreateController.java
@@ -7,13 +7,17 @@
  */
 package HI_Music_records_UI;
 
+import DAO.DataAccess;
+import Entities.SoundcloudLinks;
 import java.io.IOException;
-import javafx.event.ActionEvent;
+import java.util.List;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -39,5 +43,30 @@ public class FXMLCreateController {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLMusic.fxml"));
         Stage stage = (Stage) navMusic.getScene().getWindow();
         stage.setScene(new Scene(root));
+    }
+    
+    @FXML
+    private Text statusText;
+    @FXML
+    private TextArea textAreaCode;
+    @FXML
+    private void saveCode() throws IOException {
+        String code = textAreaCode.getText();
+        DataAccess dao = new DataAccess();
+        List<SoundcloudLinks> codes = dao.findAll();
+        boolean condition = false;
+        for(SoundcloudLinks cod : codes){
+            if(cod.getCode().equals(code)){
+                condition = true;
+                statusText.setStyle("-fx-fill: red;");
+                statusText.setText("Code already exist!");
+            }
+        }
+        if(condition == false){
+            dao.persist(code);
+            statusText.setStyle("-fx-fill: green;");
+            statusText.setText("Successfuly saved!");
+        }
+        
     }
 }
